b4c9bb8590ae098e6df8d1e9c709aab3041c7445
Alex Radulescu <radulescu.alexandru.doru@gmail.com>
2022-07-10 02:40:38 +0000

    :star: Main routing setup in RRv6 style

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,39 @@
 import { Global } from '@emotion/react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
-import { Challenge } from 'src/challenges'
 import { cssReset } from 'src/shared'
 
 export const App = () => {
   return (
-    <main>
+    <>
       <Global styles={cssReset} />
 
-      <h1>Welcome to Issho</h1>
-      <p>May consensus be with you</p>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<>Main page template</>}>
+            <Route index element={<>Home Index</>} />
+            <Route path="challenge" element={<>Challenge Root</>}>
+              <Route path=":challengeId" element={<>Challenge ID</>}>
+                <Route path="edit" element={<>Edit Challenge ID</>} />
+                <Route index element={<>Challenge ID show</>} />
+              </Route>
+              <Route path="new" element={<>New Challenge</>} />
+              <Route index element={<>All Challenges</>} />
+            </Route>
 
-      <Challenge />
-    </main>
+            <Route path="result" element={<>Results Root</>}>
+              <Route path=":resultId" element={<>Result ID</>} />
+              <Route index element={<>All Results</>} />
+            </Route>
+
+            <Route path="dashboard" element={<>Dashboard</>} />
+            <Route path="settings" element={<>Settings/Profile</>} />
+
+            <Route path="login" element={<>Login</>} />
+            <Route path="register" element={<>Register</>} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </>
   )
 }
---
+++ src/shared/pages/MainPageTemplate.tsx
@@ -0,0 +1,3 @@
+export const MainPageTemplate = () => {
+  return <>hello</>
+}
